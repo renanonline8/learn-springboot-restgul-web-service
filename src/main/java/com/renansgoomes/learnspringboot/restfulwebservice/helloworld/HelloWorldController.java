cde949849599fabc6c35f6bd91400dd05ceecedf
Renan Santos Gomes <github@renansgomes.com.br>
2023-06-05 15:22:07 -0300

    Creating a Hello World REST API with Spring Boot

---
+++ src/main/java/com/renansgoomes/learnspringboot/restfulwebservice/helloworld/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.renansgoomes.learnspringboot.restfulwebservice.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
